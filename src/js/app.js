eeda18fd5fd8328354de9b81de1c9dd15be32eac
Legata <legata93@gmail.com>
2023-10-30 21:24:03 +0300

    Release 1.0

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,21 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default class Team {
+    constructor() {
+      this.members = new Set();
+    }
+  
+    add(character) {
+      if (this.members.has(character)) {
+        throw new Error('Такой персонаж уже есть!!!');
+      } else {
+        this.members.add(character);
+      }
+    }
+  
+    addAll(...characters) {
+      characters.forEach((character) => this.members.add(character));
+    }
+  
+    toArray() {
+      return Array.from(this.members);
+    }
+  }
